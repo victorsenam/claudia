1e4f9c02c8a5110c129b32f07b816ff2101a051c
Victor Sena <victorsenam@gmail.com>
2015-10-05 11:28:48 -0300

    Revert "Usuário com autenticação e secure password"
    This reverts commit 4d4a57588e65de18070bcadce8a78dd6664a57cf.

--- db/migrate/20151004200956_add_email_confirm_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddEmailConfirmToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :email_confirmation, :string
-  end
-end
--- spec/factories/users.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    name "John Doe"
-    email "john.doe@example.com"
-    password "thisisagoodpassword,trustme"
-    type 0
-  end
-end
