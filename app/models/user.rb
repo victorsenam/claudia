97bc729554482420d8c5df58114a02d923c81e65
Victor Sena <victorsenam@gmail.com>
2015-10-05 19:03:28 -0300

    Model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+  # User ranks constants
+  PENDING = 0
+  ACCEPTED = 1
+  SUPER = 7
+
+  has_secure_password
+  validates :name, presence: true
+  validates :password, length: {minimum: 6}
+  validates :email, presence: true, confirmation: true, uniqueness: true, format: {with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/}
+
+  before_create :override_rank
+
+  def override_rank
+    self.rank = 0
+  end
 end
