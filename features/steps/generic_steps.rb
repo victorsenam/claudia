a974b2d69421acb834e4768f666b1c4d61072835
Victor Sena Molero <victorsenam@gmail.com>
2015-10-06 11:06:00 -0300

    Typo on generic steps

--- features/steps/generic_steps.rb
+++ features/steps/generic_steps.rb
@@ -7,5 +7,5 @@ When(/^I click the "(.*?)" button$/) do |button_name|
 end
 
 Then(/^I should see "(.*?)"$/) do |text|
-    expect ( page ).to have_content text )
+    expect( page ).to have_content( text )
 end
