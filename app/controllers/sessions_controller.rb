c134507159bfe48a535b9143eacc13247fcc897a
Victor Sena Molero <victorsenam@gmail.com>
2015-10-08 15:53:47 -0300

    partially built sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
   def new
+    @errors = flash['errors'] || []
+    byebug
   end
 
   def create
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -7,6 +7,20 @@ RSpec.describe SessionsController, type: :controller do
       get :new
       expect(response).to have_http_status(:success)
     end
+
+    # TODO: Pass this test :) (It's not assigning the flash correctly)
+    it "should assign errors from last request on errors", :pending do
+      errors = ['testes de erros']
+      flash['errors'] = errors
+      get :new
+      expect(assigns(:errors)).to eq( errors )
+    end
+    
+    it "should assign an empty array to errors on absence of errors" do
+      get :new
+      
+      expect(assigns(:errors)).to eq( [] )
+    end
   end
 
   describe "GET #create" do
