ff4fb344937b9650f9890b64addd4aa6e965b6e5
Pedro Nepomuceno <pedro.isn1@gmail.com>
2015-10-09 15:40:35 -0300

    Cucumber tests for teams created

---
+++ features/steps/team_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I am at the team creation page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I fill the form with a valid Team$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I try to access the team creation page$/) do
+  pending # express the regexp above with the code you wish you had
+end
