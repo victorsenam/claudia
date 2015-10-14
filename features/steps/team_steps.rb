252ad03d37116b6bbdc915b9512f88d5b0046fd0
Pedro Nepomuceno <pedro.isn1@gmail.com>
2015-10-14 16:46:50 -0300

    Team creation working. Even after error

--- features/steps/team_steps.rb
+++ features/steps/team_steps.rb
@@ -3,8 +3,8 @@ Given(/^I am at the team creation page$/) do
 end
 
 When(/^I fill the form with a valid Team$/) do
-  team = create(:team)
-  fill_in 'Nome', with: team.name
+  team = attributes_for(:team)
+  fill_in 'Nome', with: team[:name]
 end
 
 When(/^I try to access the team creation page$/) do
