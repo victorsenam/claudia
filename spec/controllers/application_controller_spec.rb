6258ea03c8ae067990059ecf3637f0241af0e277
Victor Sena Molero <victorsenam@gmail.com>
2015-11-04 13:57:46 -0200

    Little progress

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,37 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
-  describe "force_authentication" do
-    it "redirects to login page if not logged in" do
-      pending "needs review"
-      get '/users'
-      expect( response ).to redirect_to('/sessions/new')
+  describe "Filters for permission checking" do
+    controller do
+      before_action :force_authentication
+      #before_action :has_to_be_admin, only: [:admin_restricted_page]
+
+      def user_restricted_page
+        p 'yuhul'
+        render text: "Access Granted"
+      end
+
+      def admin_restricted_page
+        render text: "Access Granted"
+      end
+
     end
 
-    it "destroys expired sessions" do
-      pending 'test writing'
+    before(:example) do
+      routes.draw do
+        %w(user admin).each do |permission|
+          get "#{permission}_restricted_page" => "anonymous##{permission}_restricted_page"
+        end
+      end
     end
 
-    it "extends validation of non expired sessions" do
-      pending 'test writing'
+    describe "force_authentication" do
+      it "redirects to login page if not logged in" do
+        get :user_restricted_page
+        expect( response ).to redirect_to(sessions_new_path)
+      end
+
+      it "destroys expired sessions" do
+       from =  
+      end
+
+      it "extends validation of non expired sessions" do
+        pending 'test writing'
+      end
+
+      it "should set @current_user to the current user" do
+        pending 'test writing'
+      end
     end
 
-    it "should set @current_user to the current user" do
-      pending 'test writing'
-    end
-  end
+    describe "has_to_be_admin" do
+      it "redirects non admin users to root" do
+        pending 'test writing'
+      end
 
-  describe "has_to_be_admin" do
-    it "redirects non admin users to root" do
-      pending 'test writing'
-    end
+      it "allows admin users to admin around" do
+        pending 'test writing'
+      end
 
-    it "allows admin users to admin around" do
-      pending 'test writing'
-    end
-
-    it "allows root user to admin around" do
-      pending 'test writing'
+      it "allows root user to admin around" do
+        pending 'test writing'
+      end
     end
   end
 end
