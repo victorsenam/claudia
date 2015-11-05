ae942bf6edc2ff36c3099099b9c230f95cf9fe48
Victor Sena Molero <victorsenam@gmail.com>
2015-11-05 15:32:03 -0200

    Authentication Filters Complete :)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,24 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def force_authentication
-    redirect_to sessions_new_path
+    if !session[:auth]
+      flash[:errors] = ["Por favor, faça o login"]
+      redirect_to sessions_new_path
+    elsif session[:auth][:login_time] < 10.days.ago
+      flash[:errors] = ["Sua sessão expirou"]
+      redirect_to sessions_destroy_path
+    else
+      session[:auth][:login_time] = Time.now()
+      @current_user ||= User.find(session[:auth][:user_id])
+    end
+  end
+
+  def has_to_be_admin
+    force_authentication
+
+    if @current_user.rank < User::ADMIN
+      flash[:errors] = ["Você deve ser admin para acessar esta página"]
+      redirect_to root_path
+    end
   end
 end
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe ApplicationController, type: :controller do
       it "redirects to login page if not logged in" do
         get :user_restricted_page
 
-        expect( response ).to redirect_to(sessions_new_path)
+        expect( response ).to redirect_to( sessions_new_path )
       end
 
       it "destroys expired sessions" do
@@ -37,7 +37,7 @@ RSpec.describe ApplicationController, type: :controller do
         user.save!
         get :user_restricted_page, nil, { auth: {user_id: user.id, login_time: 1.year.ago} }        
 
-        expect( session.to_hash ).not_to have_key('auth')
+        expect( response ).to redirect_to( sessions_destroy_path )
       end
 
       it "extends validation of non expired sessions" do
@@ -46,7 +46,7 @@ RSpec.describe ApplicationController, type: :controller do
         user.save!
         get :user_restricted_page, nil, { auth: {user_id: user.id, login_time: 10.minutes.ago} }
 
-        expect( session[:auth][:login_time] ).to_be >= 5.minutes.ago
+        expect( session[:auth][:login_time] ).to be >= 5.minutes.ago
       end
 
       it "should set @current_user to the current user" do
