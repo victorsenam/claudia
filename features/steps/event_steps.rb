027a837959e33609885ca64c26c3292c7b0d773d
Victor Sena Molero <victorsenam@gmail.com>
2015-12-07 11:21:50 -0200

    Writing event steps

---
+++ features/steps/event_steps.rb
@@ -0,0 +1,12 @@
+Given(/^I (try to)? access the event creation page$/) do
+  visit '/events/new'
+end
+
+Given(/^I fill the form with a valid event$/) do
+  event = build(:event)
+  
+  fill_in 'Event', with: event.name
+  fill_in 'Date', with: event.date
+  fill_in 'Image', with: event.image_url
+  fill_in 'Description', with: event.description
+end
