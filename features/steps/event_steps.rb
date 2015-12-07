667a511c2cb8537398c0399658d9321b582fcf01
Victor Sena Molero <victorsenam@gmail.com>
2015-12-07 12:01:32 -0200

    Removing trailing space

--- features/steps/event_steps.rb
+++ features/steps/event_steps.rb
@@ -4,7 +4,7 @@ end
 
 Given(/^I fill the form with a valid event$/) do
   event = build(:event)
-  
+
   fill_in 'Event', with: event.name
   fill_in 'Date', with: event.date
   fill_in 'Image', with: event.image_url
