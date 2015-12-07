ccdcf1103cb5659144aad50546f7b5b608687965
Victor Sena Molero <victorsenam@gmail.com>
2015-12-07 18:48:44 -0200

    Event factory

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,9 +1,11 @@
+require 'faker'
+
 FactoryGirl.define do
   factory :event do
-    name "MyText"
-date "2015-12-07 11:57:07"
-image_url "MyText"
-description "MyText"
+    name Faker::Hipster.sentence(2, true, 1)
+    date Faker::Time.between(Time.new(2016, 7, 24, 12, 0, 0), Time.new(2016, 7, 30, 14, 0, 0))
+    image_url Faker::Placeholdit.image("400x400")
+    description Faker::Lorem.sentence
   end
 
 end
