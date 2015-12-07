9048175c45d4ca4429134e7d86fc5b67235c7979
Victor Sena Molero <victorsenam@gmail.com>
2015-12-07 18:49:08 -0200

    Events views passing specs

--- spec/views/events/index.html.erb_spec.rb
+++ spec/views/events/index.html.erb_spec.rb
@@ -3,23 +3,19 @@ require 'rails_helper'
 RSpec.describe "events/index", type: :view do
   before(:each) do
     assign(:events, [
-      Event.create!(
-        :name => "MyText",
-        :image_url => "MyText",
-        :description => "MyText"
-      ),
-      Event.create!(
-        :name => "MyText",
-        :image_url => "MyText",
-        :description => "MyText"
-      )
+      create(:event),
+      create(:event)
     ])
   end
 
   it "renders a list of events" do
     render
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+
+    Event.all.each do |event|
+      assert_select "tr>td", :text => event.name
+      assert_select "tr>td", :text => event.date.to_s
+      assert_select "tr>td", :text => event.image_url
+      assert_select "tr>td", :text => event.description
+    end
   end
 end
