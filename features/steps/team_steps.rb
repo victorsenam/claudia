f12ba46f302823249490168aaa8e858e7a687ac7
Victor Sena Molero <victorsenam@gmail.com>
2015-12-08 15:41:12 -0200

    Steps for team creation

--- features/steps/team_steps.rb
+++ features/steps/team_steps.rb
@@ -1,12 +1,10 @@
-Given(/^I am at the team creation page$/) do
+When(/^I(?: try to|) access the team creation page$/) do
   visit 'teams/new'
 end
 
-When(/^I fill the form with a valid Team$/) do
+When(/^I fill the form with a valid team$/) do
   team = attributes_for(:team)
   fill_in 'Nome', with: team[:name]
 end
 
-When(/^I try to access the team creation page$/) do
-  visit 'teams/new'
-end
+
