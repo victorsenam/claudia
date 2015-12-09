4156e10626733e675ee61926a5e316577c3d762e
Victor Sena Molero <victorsenam@gmail.com>
2015-12-09 18:03:20 -0200

    Passing function from helper to model

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,11 @@ class Team < ActiveRecord::Base
 
   has_and_belongs_to_many :users
   has_and_belongs_to_many :events
+
+  def set_users(users)
+    self.users.clear
+    users.each do |user_id|
+      self.users << User.find(user_id.to_i) unless user_id == ""
+    end
+  end
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -8,4 +8,36 @@ RSpec.describe Team, type: :model do
 
     it { should validate_presence_of(:name).with_message(" vazio!") }
   end
+
+  describe "refresh team's users associations" do
+    before(:each) do
+      @team = create(:team)
+      @param = []
+
+      create(:user)
+      @param << create(:user).id.to_s
+      @param << create(:user).id.to_s
+    end
+
+    it "associates every user whose id in on the array to given team" do
+      @team.set_users(@param)
+      @team.reload
+      @param.each do |el|
+        expect(@team.users).to include(User.find(el.to_i))
+      end
+    end
+
+    it "works ok when blank strings are sent on param" do
+      @param << ""
+      expect{ @team.set_users(@param) }.not_to raise_error
+    end
+
+    it "removes old associations if they are not on params" do
+      user = create(:user)
+      @team.users << user
+      @team.set_users(@param)
+      @team.reload
+      expect(@team.users).not_to include(user)
+    end
+  end
 end
