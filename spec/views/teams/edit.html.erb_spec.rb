0ddf6bc18a32b95ebccbc281cb9f4e6127f43ea8
Victor Sena Molero <victorsenam@gmail.com>
2015-12-09 11:31:00 -0200

    form for associating users with teams

--- spec/views/teams/edit.html.erb_spec.rb
+++ spec/views/teams/edit.html.erb_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "teams/edit", type: :view do
   before(:each) do
-    @team = assign(:team, Team.create!(
-      :name => "MyString"
-    ))
+    @team = assign(:team, create(:team))
+    create(:user)
+    create(:user).teams << @team
   end
 
   it "renders the edit team form" do
@@ -13,6 +13,10 @@ RSpec.describe "teams/edit", type: :view do
     assert_select "form[action=?][method=?]", team_path(@team), "post" do
 
       assert_select "input#team_name[name=?]", "team[name]"
+
+      User.all.each do |user|
+        assert_select "input#team_users_#{user.id}[name=?][value='#{user.id}']#{'[checked]' if user.teams.include?(@team)}", 'team[users][]'
+      end
     end
   end
 end
